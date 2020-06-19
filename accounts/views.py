526108c28bce6008a7fc7b5da26315d6a6aea462
docela <lade.doc@gmail.com>
2020-06-19 20:43:35 +0100

    Created the logic for the dashboard and implemented a logout button system.

--- accounts/views.py
+++ accounts/views.py
@@ -64,6 +64,9 @@ def login(request):
 
 
 def logout(request):
+    if request.method == "POST":
+      auth.logout(request)
+      messages.success(request, 'You are now logged out.')
     return redirect('index')
 
 
